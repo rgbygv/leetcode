f10ffecf337e9734a18e3f5d35880e5b96f3ac85
vthe <idashatao@gmail.com>
2024-03-29 14:23:24 +0800

    Sliding Window

--- rust/src/2962.count-subarrays-where-max-element-appears-at-least-k-times/solution.rs
+++ rust/src/2962.count-subarrays-where-max-element-appears-at-least-k-times/solution.rs
@@ -14,17 +14,36 @@ use std::mem::swap;
 
 impl Solution {
     pub fn count_subarrays(nums: Vec<i32>, k: i32) -> i64 {
+        let max = *nums.iter().max().unwrap();
+        let k = k as usize;
+        let mut pos = vec![];
+        let mut res = 0;
+        let mut last = 0;
+        for (i, x) in nums.into_iter().enumerate() {
+            if x == max {
+                pos.push(i);
+                // println!("{pos:?}");
+                let n = pos.len();
+                if n >= k {
+                    last = pos[n - k] + 1;
+                    res += last;
+                }
+            } else {
+                res += last;
+            }
+        }
 
+        res as _
     }
 }
 
 // @lc code=end
 
 fn main() -> Result<()> {
-	let nums: Vec<i32> = deserialize(&read_line()?)?;
-	let k: i32 = deserialize(&read_line()?)?;
-	let ans: i64 = Solution::count_subarrays(nums, k).into();
+    let nums: Vec<i32> = deserialize(&read_line()?)?;
+    let k: i32 = deserialize(&read_line()?)?;
+    let ans: i64 = Solution::count_subarrays(nums, k).into();
 
-	println!("\noutput: {}", serialize(ans)?);
-	Ok(())
+    println!("\noutput: {}", serialize(ans)?);
+    Ok(())
 }
