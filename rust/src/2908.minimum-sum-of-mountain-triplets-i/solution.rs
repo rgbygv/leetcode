f1607b5b25e1f6113ff78c87c99ee2a2aea55460
vthe <idashatao@gmail.com>
2024-03-29 14:03:32 +0800

    Simulation

--- rust/src/2908.minimum-sum-of-mountain-triplets-i/solution.rs
+++ rust/src/2908.minimum-sum-of-mountain-triplets-i/solution.rs
@@ -14,16 +14,33 @@ use std::mem::swap;
 
 impl Solution {
     pub fn minimum_sum(nums: Vec<i32>) -> i32 {
+        let n = nums.len();
+        let mut res = i32::MAX;
+        let mut left = nums[0];
 
+        for mid in 1..n - 1 {
+            if left < nums[mid] {
+                let right = *nums[mid + 1..n].iter().min().unwrap();
+                if right < nums[mid] {
+                    res = res.min(left + right + nums[mid])
+                }
+            }
+            left = left.min(nums[mid])
+        }
+        if res == i32::MAX {
+            -1
+        } else {
+            res
+        }
     }
 }
 
 // @lc code=end
 
 fn main() -> Result<()> {
-	let nums: Vec<i32> = deserialize(&read_line()?)?;
-	let ans: i32 = Solution::minimum_sum(nums).into();
+    let nums: Vec<i32> = deserialize(&read_line()?)?;
+    let ans: i32 = Solution::minimum_sum(nums).into();
 
-	println!("\noutput: {}", serialize(ans)?);
-	Ok(())
+    println!("\noutput: {}", serialize(ans)?);
+    Ok(())
 }
