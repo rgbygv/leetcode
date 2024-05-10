20d64c73bffb9f8df7e92496304de3944d420e66
vthe <idashatao@gmail.com>
2024-05-10 12:42:48 +0800

    update 2960: simulation

---
+++ python/2960.count-tested-devices-after-test-operations/solution.py
@@ -0,0 +1,38 @@
+# Created by Jones at 2024/05/10 12:40
+# leetgo: 1.4.7
+# https://leetcode.cn/problems/count-tested-devices-after-test-operations/
+
+from typing import *
+from leetgo_py import *
+
+from bisect import bisect_left, bisect_right
+from collections import Counter, defaultdict, deque
+from functools import cache, cmp_to_key, lru_cache, reduce
+from heapq import heapify, heappop, heappush, heappushpop, heapreplace
+from icecream import ic
+from itertools import accumulate, chain, count, pairwise, zip_longest
+from math import ceil, comb, floor, gcd, inf, isqrt, lcm, log2, perm, sqrt
+from operator import xor
+from pprint import pprint
+from string import ascii_lowercase
+from typing import List, Optional
+
+# @lc code=begin
+
+
+class Solution:
+    def countTestedDevices(self, batteryPercentages: List[int]) -> int:
+        res = 0
+        for x in batteryPercentages:
+            if x - res <= 0:
+                continue
+            res += 1
+        return res
+
+
+# @lc code=end
+
+if __name__ == "__main__":
+    batteryPercentages: List[int] = deserialize("List[int]", read_line())
+    ans = Solution().countTestedDevices(batteryPercentages)
+    print("\noutput:", serialize(ans, "integer"))
