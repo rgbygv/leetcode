1af749e75a7186fc81fd9ec8c0e00a61f0569ef3
vthe <idashatao@gmail.com>
2024-05-11 15:00:09 +0800

    update 1686: greedy

--- python/1686.stone-game-vi/solution.py
+++ python/1686.stone-game-vi/solution.py
@@ -1,5 +1,5 @@
-# Created by Jones at 2024/02/02 12:57
-# leetgo: 1.4.1
+# Created by Jones at 2024/05/11 14:29
+# leetgo: 1.4.7
 # https://leetcode.cn/problems/stone-game-vi/
 
 from typing import *
@@ -7,11 +7,11 @@ from leetgo_py import *
 
 from bisect import bisect_left, bisect_right
 from collections import Counter, defaultdict, deque
-from copy import deepcopy
 from functools import cache, cmp_to_key, lru_cache, reduce
 from heapq import heapify, heappop, heappush, heappushpop, heapreplace
+from icecream import ic
 from itertools import accumulate, chain, count, pairwise, zip_longest
-from math import ceil, comb, floor, gcd, inf, isqrt, log2, perm, sqrt
+from math import ceil, comb, floor, gcd, inf, isqrt, lcm, log2, perm, sqrt
 from operator import xor
 from pprint import pprint
 from string import ascii_lowercase
@@ -22,26 +22,34 @@ from typing import List, Optional
 
 class Solution:
     def stoneGameVI(self, aliceValues: List[int], bobValues: List[int]) -> int:
-        q = []
-        for x, y in zip(aliceValues, bobValues):
-            # q.append((max(abs(x), abs(y)), x, y))
-            item = (x + y, x, y)
-            q.append(item)
-        q.sort(reverse=True)
-
+        q = sorted(
+            ((x + y, x, y) for x, y in zip(aliceValues, bobValues)), reverse=True
+        )
         res = 0
         for i, (_, x, y) in enumerate(q):
-            if i & 1 == 0:
-                res += x
-            else:
-                res -= y
+            res += x if i & 1 == 0 else -y
+
+        # wrong
+        # @cache
+        # def dfs(i: int, turn: bool):
+        #     if i == n:
+        #         return 0
+        #     if turn:
+        #         return max(
+        #             aliceValues[i] - dfs(i + 1, not turn),
+        #             -bobValues[i] + dfs(i + 1, turn),
+        #         )
+        #     else:
+        #         return min(
+        #             bobValues[i] - dfs(i + 1, not turn),
+        #             -aliceValues[i] + dfs(i + 1, turn),
+        #         )
+
+        # res = dfs(0, True)
 
         if res > 0:
             return 1
-        elif res == 0:
-            return 0
-        else:
-            return -1
+        return -1 if res < 0 else 0
 
 
 # @lc code=end
