4981bd27ebfb6dcdb248eb0ef5dc37ac38149f08
vthe <idashatao@gmail.com>
2024-05-16 15:14:46 +0800

    update 2331: recursion

---
+++ python/2331.evaluate-boolean-binary-tree/solution.py
@@ -0,0 +1,43 @@
+# Created by Jones at 2024/05/16 15:11
+# leetgo: 1.4.7
+# https://leetcode.com/problems/evaluate-boolean-binary-tree/
+
+from typing import *
+from leetgo_py import *
+
+from bisect import bisect_left, bisect_right
+from collections import Counter, defaultdict, deque
+from functools import cache, cmp_to_key, lru_cache, reduce
+from heapq import heapify, heappop, heappush, heappushpop, heapreplace
+from icecream import ic
+from itertools import accumulate, chain, count, pairwise, zip_longest
+from math import ceil, comb, floor, gcd, inf, isqrt, lcm, log2, perm, sqrt
+from operator import xor
+from pprint import pprint
+from string import ascii_lowercase
+from typing import List, Optional
+
+# @lc code=begin
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def evaluateTree(self, root: Optional[TreeNode]) -> bool:
+        if not root.left and not root.right:
+            return bool(root.val)
+        if root.val == 2:
+            return self.evaluateTree(root.left) or self.evaluateTree(root.right)
+        return self.evaluateTree(root.left) and self.evaluateTree(root.right)
+
+
+# @lc code=end
+
+if __name__ == "__main__":
+    root: TreeNode = deserialize("TreeNode", read_line())
+    ans = Solution().evaluateTree(root)
+    print("\noutput:", serialize(ans, "boolean"))
