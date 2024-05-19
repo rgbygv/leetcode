a1a3693c755e9fd547ca3b08ae20a49a8b22f1c6
vthe <idashatao@gmail.com>
2024-05-19 15:11:42 +0800

    update 1535: simulation

--- python/1535.find-the-winner-of-an-array-game/question.md
+++ python/1535.find-the-winner-of-an-array-game/question.md
@@ -1,51 +1,43 @@
-# [1535. 找出数组游戏的赢家][link] (Medium)
+# [1535. Find the Winner of an Array Game][link] (Medium)
 
 [link]: https://leetcode.cn/problems/find-the-winner-of-an-array-game/
 
-给你一个由 **不同** 整数组成的整数数组 `arr` 和一个整数 `k` 。
+Given an integer array `arr` of **distinct** integers and an integer `k`.
 
-每回合游戏都在数组的前两个元素（即 `arr[0]` 和 `arr[1]` ）之间进行。比较 `arr[0]` 与 `arr[1]` 的大小
-，较大的整数将会取得这一回合的胜利并保留在位置 `0` ，较小的整数移至数组的末尾。当一个整数赢得 `k` 个
-连续回合时，游戏结束，该整数就是比赛的 **赢家** 。
+A game will be played between the first two elements of the array (i.e. `arr[0]` and `arr[1]`). In
+each round of the game, we compare `arr[0]` with `arr[1]`, the larger integer wins and remains at
+position `0`, and the smaller integer moves to the end of the array. The game ends when an integer
+wins `k` consecutive rounds.
 
-返回赢得比赛的整数。
-题目数据 **保证** 游戏存在赢家。
+Return the integer which will win the game.
 
-**示例 1：**
+It is **guaranteed** that there will be a winner of the game.
 
-```
-输入：arr = [2,1,3,5,4,6,7], k = 2
-输出：5
-解释：一起看一下本场游戏每回合的情况：
-
-因此将进行 4 回合比赛，其中 5 是赢家，因为它连胜 2 回合。
-```
-
-**示例 2：**
+**Example 1:**
 
 ```
-输入：arr = [3,2,1], k = 10
-输出：3
-解释：3 将会在前 10 个回合中连续获胜。
+Input: arr = [2,1,3,5,4,6,7], k = 2
+Output: 5
+Explanation: Let's see the rounds of the game:
+Round |       arr       | winner | win_count
+  1   | [2,1,3,5,4,6,7] | 2      | 1
+  2   | [2,3,5,4,6,7,1] | 3      | 1
+  3   | [3,5,4,6,7,1,2] | 5      | 1
+  4   | [5,4,6,7,1,2,3] | 5      | 2
+So we can see that 4 rounds will be played and 5 is the winner because it wins 2 consecutive games.
 ```
 
-**示例 3：**
+**Example 2:**
 
 ```
-输入：arr = [1,9,8,2,3,7,6,4,5], k = 7
-输出：9
+Input: arr = [3,2,1], k = 10
+Output: 3
+Explanation: 3 will win the first 10 rounds consecutively.
 ```
 
-**示例 4：**
+**Constraints:**
 
-```
-输入：arr = [1,11,22,33,44,55,66,77,88,99], k = 1000000000
-输出：99
-```
-
-**提示：**
-
-- `2 <= arr.length <= 10^5`
-- `1 <= arr[i] <= 10^6`
-- `arr` 所含的整数 **各不相同** 。
-- `1 <= k <= 10^9`
+- `2 <= arr.length <= 10⁵`
+- `1 <= arr[i] <= 10⁶`
+- `arr` contains **distinct** integers.
+- `1 <= k <= 10⁹`
--- python/1535.find-the-winner-of-an-array-game/solution.py
+++ python/1535.find-the-winner-of-an-array-game/solution.py
@@ -1,5 +1,5 @@
-# Created by Jones at 2023/11/05 12:12
-# leetgo: dev
+# Created by Jones at 2024/05/19 15:06
+# leetgo: 1.4.7
 # https://leetcode.cn/problems/find-the-winner-of-an-array-game/
 
 from typing import *
@@ -7,39 +7,45 @@ from leetgo_py import *
 
 from bisect import bisect_left, bisect_right
 from collections import Counter, defaultdict, deque
-from copy import deepcopy
 from functools import cache, cmp_to_key, lru_cache, reduce
 from heapq import heapify, heappop, heappush, heappushpop, heapreplace
+from icecream import ic
 from itertools import accumulate, chain, count, pairwise, zip_longest
-from math import ceil, comb, floor, gcd, inf, isqrt, log2, perm, sqrt
+from math import ceil, comb, floor, gcd, inf, isqrt, lcm, log2, perm, sqrt
 from operator import xor
+from pprint import pprint
 from string import ascii_lowercase
 from typing import List, Optional
 
 # @lc code=begin
-# from sortedcontainers import SortedList
 
 
 class Solution:
     def getWinner(self, arr: List[int], k: int) -> int:
+        """
+        - `2 <= arr.length <= 10⁵`
+        - `1 <= arr[i] <= 10⁶`
+        - `arr` contains **distinct** integers.
+        - `1 <= k <= 10⁹`
+        """
         n = len(arr)
-
-        if k >= n - 1:
+        if k >= n:
             return max(arr)
-
-        pre = arr[0]
+        mx = max(arr)
         cnt = 0
-        for i in range(1, n):
+        last = arr[0]
+        for i in range(1, len(arr)):
             x = arr[i]
-            if x > pre:
-                pre = x
-                cnt = 1
-            else:
+            if last > x:
                 cnt += 1
-            if cnt >= k:
-                return pre
-
-        return pre
+            else:
+                if x == mx:
+                    return x
+                last = x
+                cnt = 1
+            if cnt == k:
+                return last
+        return -1
 
 
 # @lc code=end
@@ -48,5 +54,4 @@ if __name__ == "__main__":
     arr: List[int] = deserialize("List[int]", read_line())
     k: int = deserialize("int", read_line())
     ans = Solution().getWinner(arr, k)
-
-    print("\noutput:", serialize(ans))
+    print("\noutput:", serialize(ans, "integer"))
