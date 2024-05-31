b3e5bb1707274c93623d214fa51f0be9c0af4fa0
vthe <idashatao@gmail.com>
2024-05-31 09:24:08 +0800

    update 0260: xor

--- python/0260.single-number-iii/question.md
+++ python/0260.single-number-iii/question.md
@@ -1,36 +1,38 @@
-# [260. 只出现一次的数字 III][link] (Medium)
+# [260. Single Number III][link] (Medium)
 
-[link]: https://leetcode.cn/problems/single-number-iii/
+[link]: https://leetcode.com/problems/single-number-iii/
 
-给你一个整数数组 `nums`，其中恰好有两个元素只出现一次，其余所有元素均出现两次。 找出只出现一次的那两
-个元素。你可以按 **任意顺序** 返回答案。
+Given an integer array `nums`, in which exactly two elements appear only once and all the other
+elements appear exactly twice. Find the two elements that appear only once. You can return the
+answer in **any order**.
 
-你必须设计并实现线性时间复杂度的算法且仅使用常量额外空间来解决此问题。
+You must write an algorithm that runs in linear runtime complexity and uses only constant extra
+space.
 
-**示例 1：**
-
-```
-输入：nums = [1,2,1,3,2,5]
-输出：[3,5]
-解释：[5, 3] 也是有效的答案。
-```
-
-**示例 2：**
+**Example 1:**
 
 ```
-输入：nums = [-1,0]
-输出：[-1,0]
+Input: nums = [1,2,1,3,2,5]
+Output: [3,5]
+Explanation:  [5, 3] is also a valid answer.
 ```
 
-**示例 3：**
+**Example 2:**
 
 ```
-输入：nums = [0,1]
-输出：[1,0]
+Input: nums = [-1,0]
+Output: [-1,0]
 ```
 
-**提示：**
+**Example 3:**
+
+```
+Input: nums = [0,1]
+Output: [1,0]
+```
+
+**Constraints:**
 
 - `2 <= nums.length <= 3 * 10⁴`
 - `-2³¹ <= nums[i] <= 2³¹ - 1`
-- 除两个只出现一次的整数外， `nums` 中的其他数字都出现两次
+- Each integer in `nums` will appear twice, only two integers will appear once.
--- python/0260.single-number-iii/solution.py
+++ python/0260.single-number-iii/solution.py
@@ -1,24 +1,36 @@
-# Created by Jones at 2023/10/16 14:41
-# leetgo: dev
-# https://leetcode.cn/problems/single-number-iii/
+# Created by Jones at 2024/05/31 09:05
+# leetgo: 1.4.7
+# https://leetcode.com/problems/single-number-iii/
 
 from typing import *
 from leetgo_py import *
 
-import sys 
-sys.path.append("..") 
-from _LEETCODE import *
+from bisect import bisect_left, bisect_right
+from collections import Counter, defaultdict, deque
+from functools import cache, cmp_to_key, lru_cache, reduce
+from heapq import heapify, heappop, heappush, heappushpop, heapreplace
+from icecream import ic
+from itertools import accumulate, chain, count, pairwise, zip_longest
+from math import ceil, comb, floor, gcd, inf, isqrt, lcm, log2, perm, sqrt
+from operator import xor
+from pprint import pprint
+from string import ascii_lowercase
+from typing import List, Optional
 
 # @lc code=begin
-# from sortedcontainers import SortedList
+
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
-        x = reduce(xor, nums)
-        
-        lowbit = x & -x
-        
-        x,y = 0, 0
+        """
+        if this two number is x,y
+        then x^y = xor(nums)
+        if any bit of (x^y) is 1
+        then x[bit] is diff y[bit], we can divide nums by it
+        """
+        xy = reduce(xor, nums)
+        lowbit = xy & (-xy)
+        x, y = 0, 0
         for num in nums:
             if num & lowbit != 0:
                 x ^= num
@@ -32,5 +44,4 @@ class Solution:
 if __name__ == "__main__":
     nums: List[int] = deserialize("List[int]", read_line())
     ans = Solution().singleNumber(nums)
-
-    print("\noutput:", serialize(ans))
+    print("\noutput:", serialize(ans, "integer[]"))
